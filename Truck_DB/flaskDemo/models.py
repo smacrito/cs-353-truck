6ab92c1b9fe2365310cde09272d933198d357ef6
passienter <kenzo@scheerlinck.be>
2019-04-29 20:25:56 -0500

    SAVE THIS ONE ITS THE BEST

--- Truck_DB/flaskDemo/models.py
+++ Truck_DB/flaskDemo/models.py
@@ -1,31 +1,3 @@
-<<<<<<< HEAD
-from datetime import datetime
-from flaskDemo import db, login_manager
-from flask_login import UserMixin
-from functools import partial
-from sqlalchemy import orm
-
-db.Model.metadata.reflect(db.engine)
-
-class Customer(db.Model):
-    __table__ = db.Model.metadata.tables['customer']
-
-@login_manager.user_loader
-def load_user(user_id):
-    return Customer.query.get(int(user_id)) #customerid
-
-class Employee(db.Model):
-    __table__ = db.Model.metadata.tables['employee']
-
-class Purchase(db.Model):
-    __table__ = db.Model.metadata.tables['purchase']
-
-class Test_Drive(db.Model):
-    __table__ = db.Model.metadata.tables['test_drive']
-
-class Vehicle(db.Model):
-    __table__ = db.Model.metadata.tables['vehicle']
-=======
 from datetime import datetime
 from flaskDemo import db, login_manager
 from flask_login import UserMixin
@@ -58,4 +30,3 @@ class Test_Drive(db.Model):
 
 class Vehicle(db.Model):
     __table__ = db.Model.metadata.tables['vehicle']
->>>>>>> temp
